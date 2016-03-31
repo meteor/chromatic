74f33c23c4a5a80e2cdb4bf8fae2af7c0184fe1b
Zhenya <eugeneas@gmail.com>
2016-03-31 14:03:40 -0700

    code block

---
+++ packages/code-block/CodeBlock.jsx
@@ -0,0 +1,35 @@
+/* global CodeBlock:true */
+/* global React hljs classnames */
+
+const {Chromatic} = Package['mdg:chromatic-api'] || {};
+
+CodeBlock = React.createClass({
+  propTypes: {
+    code: React.PropTypes.string,
+    className: React.PropTypes.string
+  },
+  render() {
+    const {code, className, ...other} = this.props;
+    const formattedCode = hljs.highlightAuto(code);
+    return (
+      <pre {...other} className={classnames('code-block', className)}
+        dangerouslySetInnerHTML={{__html: formattedCode.value}} />
+    );
+  }
+});
+
+
+if (Chromatic) {
+  Chromatic.add(CodeBlock, {
+    specs: [
+      new Chromatic.Spec('default', {props: {
+        code: `{ "json": "value" };
+        const square = function(x) { return Math.pow(x, 2); }
+        // comment`
+      }}),
+      new Chromatic.Spec('one-line', {props: {
+        code: `const clamp = (v, min, max) => v < min ? min : v > max ? max : v;`
+      }}),
+    ]
+  });
+}
