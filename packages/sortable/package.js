d9fb454afbe66f244e6913a5c412f02ad4eb39d6
Danielle Man <daniman0808@gmail.com>
2017-08-07 11:05:34 -0700

    remove chromatic dependency from mdg:sortable

--- packages/sortable/package.js
+++ packages/sortable/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mdg:sortable',
-  version: '0.2.11',
+  version: '0.2.12',
   summary: 'Components to create sortable things',
   git: 'https://github.com/meteor/chromatic',
   documentation: null
@@ -11,7 +11,6 @@ Package.onUse(function(api) {
   api.use([
     'ecmascript',
     'mdg:borealis@0.2.5',
-    'mdg:chromatic@0.2.6',
   ], 'client');
   api.addFiles(['Sortable.jsx', 'SortableTableExample.jsx'], 'client');
   api.export('Sortable', 'client');
