202db4846bb2b7d5b15834272a3ad57bdfd85bf9
Danielle Man <daniman0808@gmail.com>
2017-08-07 11:03:23 -0700

    remove chromatic dependency from mdg:list

--- packages/list/package.js
+++ packages/list/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mdg:list',
-  version: '0.2.13',
+  version: '0.2.14',
   summary: 'A infinite-scroll list component',
   git: 'https://github.com/meteor/chromatic',
   documentation: null
@@ -11,7 +11,6 @@ Package.onUse(function(api) {
   api.use([
     'ecmascript',
     'mdg:borealis@0.2.5',
-    'mdg:chromatic@0.2.6',
     'underscore',
     'mdg:form-components@0.2.7',
     'mdg:animations@0.2.3'
