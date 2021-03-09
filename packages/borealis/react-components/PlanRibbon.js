c4397f0fff0bae4144bcc19a942b665b532f53e3
Renan Castro <renanccastro@gmail.com>
2021-03-09 12:03:32 -0300

    #511 Meteor Cloud Updates

--- packages/borealis/react-components/PlanRibbon.js
+++ packages/borealis/react-components/PlanRibbon.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 PlanRibbon = function({ children }) {
   let className = 'tertiary';
-  if (children === 'professional') {
+  if (children.toLowerCase() === 'professional') {
     className = 'primary';
-    // eslint-disable-next-line no-param-reassign
-    children = 'PRO';
-  } else if (children === 'essentials') {
+  } else if (children.toLowerCase() === 'essentials') {
     className = 'secondary';
   }
   return <div className={`ribbon ${className}`}>{children}</div>;
